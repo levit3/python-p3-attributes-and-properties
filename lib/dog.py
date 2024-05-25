aec82c9ebfd86c19188306ae2048f0f798c2eeda
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-05-25 16:24:41 +0300

    creates class, properties

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,29 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+    def __init__ (self, name="Bravo", breed = "Pug") :
+        self.name = name
+        self.breed = breed
+    
+    def set_name(self, name):
+        if (type(name) is str and 25 > len(name) > 0) :
+            self._name = name.title()
+        else :
+            print("Name must be string between 1 and 25 characters.")
+            
+    def get_name(self):
+        return self._name
+    
+    def set_breed(self, breed):
+        if (breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            
+    def get_breed(self):
+        return self._breed
+    
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+    
+
